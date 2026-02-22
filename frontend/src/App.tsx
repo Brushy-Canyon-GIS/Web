2fe05c125cd44312d5722ae62e81d81c250a911e
Chris Giere <cjgiere99@gmail.com>
2026-02-22 13:34:10 -0700

    Change back to https

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import FeatureDetails from "./components/FeatureDetails";
 
 // const API_BASE_URL = import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:8000";
 
-const API_BASE_URL = "http://18.222.134.134:8000";
+const API_BASE_URL = "https://18.222.134.134:8000";
 
 type Layer =
   | "atlas_maps"
