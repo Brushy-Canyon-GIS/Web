19295ffc3ba3a0e42f1150a6a555a98d4c471498
Chris Giere <cjgiere99@gmail.com>
2026-03-06 15:16:03 -0700

    update __init__.py file under routers

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -3,7 +3,7 @@ Service layer for business logic.
 """
 from .geologic_service import GeologicDataService
 from .photos_service import PhotosService
+from .cross_plot_service import CrossPlotService
 
-
-__all__ = ["GeologicDataService", "PhotosService"]
+__all__ = ["GeologicDataService", "PhotosService", "CrossPlotService"]
 
