e48f3c050a19fd3aef560b05422be02109d73107
Chris Giere <cjgiere99@gmail.com>
2026-03-20 11:54:57 -0600

    Update to database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,3 +4,8 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 database = Database(DATABASE_URL)
 
+# Disable asyncpg prepared statements to work with PgBouncer
+database = Database(
+    DATABASE_URL,
+    connect_args={"statement_cache_size": 0}
+)
